65096780dc0f2021de3ca977f083fda4a4cd331c
knarula2099 <kanarula@ucsd.edu>
2022-04-22 21:16:44 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,6 +12,10 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while (currentIndex < markdown.length()) {
+            if (!markdown.contains("[") || !markdown.contains("]") || !markdown.contains("(")
+                    || !markdown.contains(")")) {
+                break;
+            }
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
